5ae97e3b1b54e838d153041ce356b9fc686496f4
Gonzalo Rojas <gonzalo.rojas@nullplatform.io>
2026-01-15 15:03:24 -0300

    fix(eks): update terraform-aws-modules/eks version constraint

--- infrastructure/aws/eks/main.tf
+++ infrastructure/aws/eks/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = ">= 21.15, < 22.0"
 
   name               = var.cluster_name
   kubernetes_version = var.kubernetes_version
